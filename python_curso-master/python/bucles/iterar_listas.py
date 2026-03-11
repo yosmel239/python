589833b78abe96d1f93f0b68bffd96b9df930bb5
matos <yosmel239@gmail.com>
2026-03-10 19:51:43 -0500

    hihihija\

---
+++ python_curso-master/python/bucles/iterar_listas.py
@@ -0,0 +1,20 @@
+animales = ["perro", "gato", "conejo", "hamster"]
+numeros = [2, 2, 3, 4, 5]
+
+#recorriendo la lista de animales
+for animal in animales:
+    print(animal)
+
+
+#recorriendo la lista de numeros multiplicado por 10    
+for numero in numeros:
+    numeros_10 = numero * 10
+    print(numeros_10)
+    
+    
+#recorriendo ambos listas al mismo tiempo con la función zip
+for animal, numero in zip(animales, numeros):
+    print(f"el : {animal} , tiene: {numero} años")    
+    
+for distancia in range(1,3):
+    print(f"la distancia es: {distancia} km")    
--- python_curso-master/python/variable2/duccionarios.py
+++ python_curso-master/python/variable2/duccionarios.py
@@ -1,6 +1,16 @@
 #creando un diccionario con dict()
 diccionario = dict(nombre="yosmel",apellido="matos,palomino")
 
-print (diccionario)
+#las listas no pueden ser claves
 
+diccionario1 = {frozenset(["shair","palomino"]):"es huapo"}
+
+#creando diccionarios con fromkeys
+diccionario2 = dict.fromkeys(["nombre","apellido"])
+diccionario3 = dict.fromkeys(["nombre","apellido"],"desconocido")
+
+
+print(diccionario2)
+print(diccionario3)
+print(diccionario1)
 print(diccionario["nombre"])
